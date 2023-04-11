960b21357b98c34f648cb2d990da6217a2042abd
Prado <mahery.rsh@gmail.com>
2023-04-11 15:37:08 +0200

    chore: define container dimensions

--- admin/tailwind.config.cjs
+++ admin/tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
     fontFamily: {
       inter: ["Inter", "sans-serif"],
     },
@@ -17,6 +24,17 @@ module.exports = {
       },
       animation: {
         "fade-in": "fadeIn .1s ease-in",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "1.5rem",
+        md: "2.5rem",
+        lg: "4rem",
+        xl: "4rem",
+        "2xl": "6rem",
       },
     },
   },
