5ba18868c7d947f8e2d0ec1adadfe6b6e54ae419
Prado <mahery.rsh@gmail.com>
2023-04-16 14:31:19 +0200

    chore: review client docker image

--- client/src/components/account/OrderCard.tsx
+++ client/src/components/account/OrderCard.tsx
@@ -10,7 +10,7 @@ const OrderCard = ({ order }: OrderCardProps) => {
   const { download, isLoading: isDownloading } = useDownloadContext();
 
   const downloadInvoice = () =>
-    download(`http://localhost:3000/invoice/${order.reference}`, {
+    download(`${import.meta.env.VITE_API_URL}/invoice/${order.reference}`, {
       filename: generateInvoiceFilename(order),
     });
 
