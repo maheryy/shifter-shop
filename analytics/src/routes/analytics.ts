095c7ce93c9e413f3abf21bc63fe2ed85e4ec662
Prado <mahery.rsh@gmail.com>
2023-05-09 11:45:05 +0200

    chore: init analytics tracker middleware

--- analytics/src/routes/analytics.ts
+++ analytics/src/routes/analytics.ts
@@ -4,14 +4,15 @@ import { UAParser } from "ua-parser-js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/test", async (req, res) => {
   const userAgent = UAParser(req.headers["user-agent"]);
   const data = await db.collection("test").find().toArray();
+  req.sendEvent({ type: "test", data: { userAgent } });
 
   res.json([data, userAgent]);
 });
 
-router.post("/", async (req, res) => {
+router.post("/event", async (req, res) => {
   res.send("Hello from analytics");
 });
 
