b9690c68ba994ec046cdbcb84be937fd9cef48d4
3kezoh <3kezoh@gmail.com>
2023-05-10 00:14:30 +0200

    feat(terraform): add secrets

---
+++ terraform/secrets.tf
@@ -0,0 +1,88 @@
+resource "google_secret_manager_secret" "jwt_secret" {
+  secret_id = "jwt_secret"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "jwt_secret_version" {
+  secret = google_secret_manager_secret.jwt_secret.id
+
+  secret_data = var.jwt_secret
+}
+
+resource "google_secret_manager_secret" "stripe_private_key" {
+  secret_id = "stripe_private_key"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "stripe_private_key_version" {
+  secret = google_secret_manager_secret.stripe_private_key.id
+
+  secret_data = var.stripe_private_key
+}
+
+
+resource "google_secret_manager_secret" "stripe_public_key" {
+  secret_id = "stripe_public_key"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "stripe_public_key_version" {
+  secret = google_secret_manager_secret.stripe_public_key.id
+
+  secret_data = var.stripe_public_key
+}
+
+resource "google_secret_manager_secret" "stripe_webhook_key" {
+  secret_id = "stripe_webhook_key"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "stripe_webhook_key_version" {
+  secret = google_secret_manager_secret.stripe_webhook_key.id
+
+  secret_data = var.stripe_webhook_key
+}
+
+resource "google_secret_manager_secret" "database_url" {
+  secret_id = "database_url"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_version" {
+  secret = google_secret_manager_secret.database_url.id
+
+  secret_data = var.database_url
+}
+
+resource "google_secret_manager_secret" "mailer_dsn" {
+  secret_id = "mailer_dsn"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "mailer_dsn_version" {
+  secret = google_secret_manager_secret.mailer_dsn.id
+
+  secret_data = var.mailer_dsn
+}
+
+
+
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,33 @@ variable "region" {
 variable "zone" {
   type = string
 }
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_private_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_public_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_webhook_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "database_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "mailer_dsn" {
+  type      = string
+  sensitive = true
+}
