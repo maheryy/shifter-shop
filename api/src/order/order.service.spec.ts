6279739c8ef5b16011eb9457c2ccb000afd616f8
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2023-05-11 01:36:03 +0200

    ✅ feat(route): Fix Review and Order tests

--- api/src/order/order.service.spec.ts
+++ api/src/order/order.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('OrderService', () => {
   let service: OrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [OrderService],
     }).compile();
 
