fe020945dbeb886ceb88983add4140be19204c5f
Prado <mahery.rsh@gmail.com>
2023-06-04 01:14:04 +0200

    chore(gateway): improve headers security with helmet

--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { Registry } from "@shifter-shop/registry";
 import { registerRoutes } from "utils/proxy";
+import helmet from "helmet";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.disable("x-powered-by");
+app.use(helmet());
 app.use(registerRoutes(Registry.getServices()));
 
 app.listen(PORT, () => {
