8572be29664f7beb10954ab8d6ab2df7bb2b74a4
Prado <mahery.rsh@gmail.com>
2023-06-18 23:59:47 +0200

    feat: add common errors lib

---
+++ libs/errors/src/index.ts
@@ -0,0 +1,8 @@
+export { HttpError } from "./lib/exceptions/HttpError";
+export { BadRequestError } from "./lib/exceptions/BadRequestError";
+export { ForbiddenError } from "./lib/exceptions/ForbiddenError";
+export { NotFoundError } from "./lib/exceptions/NotFoundError";
+export { UnauthorizedError } from "./lib/exceptions/UnauthorizedError";
+export { InternalServerError } from "./lib/exceptions/InternalServerError";
+export { ServiceUnavailableError } from "./lib/exceptions/ServiceUnavailableError";
+export { exceptionHandler } from "./lib/express/exceptionHandler";
