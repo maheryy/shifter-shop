6c264133b33c7c9057eedf1f844c7a718bbfdfb5
Prado <mahery.rsh@gmail.com>
2023-07-02 23:52:27 +0200

    refactor: remove old api nest app

--- api/src/auth/configs/jwt.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import { JwtModuleOptions } from '@nestjs/jwt';
-
-const jwtConfig: JwtModuleOptions = {
-  secret: process.env.JWT_SECRET,
-};
-
-export default registerAs('jwt', () => jwtConfig);
--- api/src/encryption/configs/bcrypt.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import { BcryptConfig } from 'src/encryption/interfaces/bcryptConfig.interface';
-
-const bcryptConfig: BcryptConfig = {
-  saltOrRounds: parseInt(process.env.BCRYPT_SALT_OR_ROUNDS || '10', 10),
-};
-
-export default registerAs('bcrypt', () => bcryptConfig);
