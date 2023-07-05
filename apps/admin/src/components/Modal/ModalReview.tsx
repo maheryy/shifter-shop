a6f6560b81163fd11a02067493698e416e6acefc
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2023-07-06 00:42:31 +0200

    ui(admin): 💄 Stylize modals

--- apps/admin/src/components/Modal/ModalReview.tsx
+++ apps/admin/src/components/Modal/ModalReview.tsx
@@ -1,39 +1,41 @@
 import {
   TFullReview,
 } from "@shifter-shop/dictionary";
+import StarIcon from "@icons/star.svg";
 
 const ModalReview = ({ review }: ModalReviewProps) => {
   let date = new Date(review.date);
 
+  let stars = [];
+
+  let filledStars = review.rating;
+
+  for (let i = 0; i < 5; i++) {
+    if (filledStars > 0) {
+      stars.push(<StarIcon key={i} className="inline-block w-7 h-7 text-purple-600" />);
+      filledStars--;
+    } else {
+      stars.push(<StarIcon key={i} className="inline-block w-7 h-7 text-gray-400" />);
+    }
+  }
+
   return (
     <>
-      {/* Modal title */}
-      <p className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300">
+      <p className="text-2xl font-bold mb-4 text-gray-700 dark:text-gray-300">
         {review.title}
       </p>
-      {/* Modal details */}
-      <p className="text-sm text-gray-700 dark:text-gray-400">
+      <p className="mt-2 mb-4 text-lg font-semibold text-gray-800 dark:text-white">
+        {stars}
+      </p>
+      <p className="text-lg text-gray-700 dark:text-gray-400">
         {review.details}
       </p>
-      {/* Modal rating */}
-      <p className="mt-2 text-lg font-semibold text-gray-800 dark:text-white">
-        Rating: {review.rating}
-      </p>
-      {/* Modal product */}
-      <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        Product: {review.product.name}
-      </p>
-      {/* Modal author */}
-      <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        Published by: {review.author.firstname} {review.author.lastname} ({review.author.email})
-      </p>
-      {/* Modal status */}
-      <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        Status: {review.status}
-      </p>
-      <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        Published on {date.toLocaleDateString()} at {date.toLocaleTimeString()}
-      </p>
+      <div className="border-t-4 pt-4 mt-4 text-lg text-gray-700 dark:text-gray-400">
+        <p><strong>Product:</strong> {review.product.name}</p>
+        <p><strong>Published by:</strong> {review.author.firstname} {review.author.lastname} ({review.author.email})</p>
+        <p><strong>Status:</strong> {review.status}</p>
+        <p><strong>Published on:</strong> {date.toLocaleDateString()} at {date.toLocaleTimeString()}</p>
+      </div>
     </>
   );
 }
