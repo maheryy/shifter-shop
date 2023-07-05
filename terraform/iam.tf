5424979f336da564bc5d7ce71d856e3893f9829c
Prado <mahery.rsh@gmail.com>
2023-07-05 12:57:04 +0200

    chore(terraform): gke node autoscaling and set new secrets

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -32,15 +32,56 @@ resource "google_secret_manager_secret_iam_member" "stripe_webhook_key_iam_membe
   member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
 }
 
-resource "google_secret_manager_secret_iam_member" "database_url_iam_member" {
+resource "google_secret_manager_secret_iam_member" "database_url_users_iam_member" {
   role      = "roles/secretmanager.secretAccessor"
-  secret_id = google_secret_manager_secret.database_url.id
+  secret_id = google_secret_manager_secret.database_url_users.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_products_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_products.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_categories_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_categories.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_reviews_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_reviews.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_orders_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_orders.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_cart_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_cart.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_profiles_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_profiles.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+resource "google_secret_manager_secret_iam_member" "database_url_inventory_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.database_url_inventory.id
   member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "mailer_dsn_iam_member" {
   role      = "roles/secretmanager.secretAccessor"
   secret_id = google_secret_manager_secret.mailer_dsn.id
+  member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "amqp_url_iam_member" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = google_secret_manager_secret.amqp_url.id
   member    = "serviceAccount:${google_service_account.shifter_shop_api.email}"
 }
 
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -55,18 +55,116 @@ resource "google_secret_manager_secret_version" "stripe_webhook_key_version" {
   secret_data = var.stripe_webhook_key
 }
 
-resource "google_secret_manager_secret" "database_url" {
-  secret_id = "database-url"
+resource "google_secret_manager_secret" "database_url_users" {
+  secret_id = "database-url-users"
 
   replication {
     automatic = true
   }
 }
 
-resource "google_secret_manager_secret_version" "database_url_version" {
-  secret = google_secret_manager_secret.database_url.id
+resource "google_secret_manager_secret_version" "database_url_users_version" {
+  secret = google_secret_manager_secret.database_url_users.id
 
-  secret_data = var.database_url
+  secret_data = var.database_url_users
+}
+
+resource "google_secret_manager_secret" "database_url_products" {
+  secret_id = "database-url-products"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_products_version" {
+  secret = google_secret_manager_secret.database_url_products.id
+
+  secret_data = var.database_url_products
+}
+
+resource "google_secret_manager_secret" "database_url_categories" {
+  secret_id = "database-url-categories"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_categories_version" {
+  secret = google_secret_manager_secret.database_url_categories.id
+
+  secret_data = var.database_url_categories
+}
+
+resource "google_secret_manager_secret" "database_url_reviews" {
+  secret_id = "database-url-reviews"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_reviews_version" {
+  secret = google_secret_manager_secret.database_url_reviews.id
+
+  secret_data = var.database_url_reviews
+}
+
+resource "google_secret_manager_secret" "database_url_orders" {
+  secret_id = "database-url-orders"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_orders_version" {
+  secret = google_secret_manager_secret.database_url_orders.id
+
+  secret_data = var.database_url_orders
+}
+
+resource "google_secret_manager_secret" "database_url_cart" {
+  secret_id = "database-url-cart"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_cart_version" {
+  secret = google_secret_manager_secret.database_url_cart.id
+
+  secret_data = var.database_url_cart
+}
+
+resource "google_secret_manager_secret" "database_url_profiles" {
+  secret_id = "database-url-profiles"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_profiles_version" {
+  secret = google_secret_manager_secret.database_url_profiles.id
+
+  secret_data = var.database_url_profiles
+}
+
+resource "google_secret_manager_secret" "database_url_inventory" {
+  secret_id = "database-url-inventory"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "database_url_inventory_version" {
+  secret = google_secret_manager_secret.database_url_inventory.id
+
+  secret_data = var.database_url_inventory
 }
 
 resource "google_secret_manager_secret" "mailer_dsn" {
@@ -83,6 +181,17 @@ resource "google_secret_manager_secret_version" "mailer_dsn_version" {
   secret_data = var.mailer_dsn
 }
 
+resource "google_secret_manager_secret" "amqp_url" {
+  secret_id = "amqp-url"
 
+  replication {
+    automatic = true
+  }
+}
 
+resource "google_secret_manager_secret_version" "amqp_url_version" {
+  secret = google_secret_manager_secret.amqp_url.id
+
+  secret_data = var.amqp_url
+}
 
